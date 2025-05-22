43131455f4c933c016f6202134fc057186b9b787
Erlend Withammer <erlendwe.ewe@gmail.com>
2025-05-22 18:37:35 +0200

    added autocomplete++

--- lua/erlenwit/core/keybinds.lua
+++ lua/erlenwit/core/keybinds.lua
@@ -54,7 +54,7 @@ keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy f
 keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files"  })
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd"  })
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor in cwd"  })
-
+keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 -- Auto-Session restore
 keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd"  })
 keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for cwd"  })
